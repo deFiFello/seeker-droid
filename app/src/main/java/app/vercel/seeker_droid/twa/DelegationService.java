0badbae30a09cf61d35a289b8149dd528aeff413
Chris Hull <chris21hull@gmail.com>
2026-03-15 15:34:46 -0400

    fix: MWA flow, local icons, asset links, rebuilt APK v1.1.0

---
+++ app/src/main/java/app/vercel/seeker_droid/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.seeker_droid.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
